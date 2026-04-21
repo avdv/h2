6db76b3ee711f43841173fcccf0ef2c13efbabf8
Claudio Bley <claudio.bley@tweag.io>
2026-04-21 11:19:07 +0200

    Debug resets

--- src/proto/streams/streams.rs
+++ src/proto/streams/streams.rs
@@ -352,6 +352,7 @@ impl<B> DynStreams<'_, B> {
         me.recv_data(self.peer, self.send_buffer, frame)
     }
 
+    #[track_caller]
     pub fn recv_reset(&mut self, frame: frame::Reset) -> Result<(), Error> {
         let mut me = self.inner.lock().unwrap();
 
@@ -592,6 +593,7 @@ impl Inner {
         })
     }
 
+    #[track_caller]
     fn recv_reset<B>(
         &mut self,
         send_buffer: &SendBuffer<B>,
